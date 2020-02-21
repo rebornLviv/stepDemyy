6d6355416dc7276f90613ebfd0c9817ec8e58742
Vlad <vladprodan6@gmail.com>
2020-02-21 13:46:40 +0200

    some changes

--- src/App.vue
+++ src/App.vue
@@ -1,206 +1,157 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="white"
-      dark
-      
-    >
-  <div class="black--text font-italic mr-5 "><router-link to="/" tag="h2" class="font-weight-regular logo" >Stepdemy</router-link></div>
-  
-<v-menu   offset-y >
-  <template v-slot:activator="{on}">
-  <v-btn 
-  text
-  
+    <v-app-bar app color="white" dark>
+      <div class="black--text font-italic mr-5">
+        <router-link to="/" tag="h2" class="font-weight-regular logo">Stepdemy</router-link>
+      </div>
 
-  v-on="on"
-  > 
-  <span class="black--text">Категорії</span>
+      <v-menu offset-y>
+        <template v-slot:activator="{on}">
+          <v-btn text v-on="on">
+            <span class="black--text">Категорії</span>
 
-    <v-icon
-   class="grey--text" 
-    >{{idef}}</v-icon>
-  </v-btn>
-      
-    
-  </template>
-  <v-list >
-    <v-list-item v-for="cat in categories" :key="cat" :to="'/courses/' + cat">{{cat}}</v-list-item>
-
-  </v-list>
-</v-menu>
+            <v-icon class="grey--text">{{idef}}</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="cat in categories" :key="cat" :to="'/courses/' + cat">{{cat}}</v-list-item>
+        </v-list>
+      </v-menu>
       <v-spacer></v-spacer>
 
-      <v-btn
-        text
-        class="grey--text"
-      >
-        
+      <v-btn text class="grey--text">
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
-      <v-btn
-      v-if="!isUserLoggedIn"
-      to="/login"
-      text
-      class="black--text"
-      >Вхід</v-btn>
+      <v-btn v-if="!isUserLoggedIn" to="/login" text class="black--text">Вхід</v-btn>
+      
 
-      <v-btn
-      v-if="isUserLoggedIn"
-      to="/"
-      text
-      @click="onLogout"
-      class="black--text"
-      >Вийти</v-btn>
+      <v-btn v-if="isUserLoggedIn" to="/" text @click="onLogout" class="black--text">Вийти</v-btn>
     </v-app-bar>
-<v-content>
-  <router-view></router-view>
-</v-content>
-<template v-if="error">
-    <v-snackbar
-    color="dark"
-    :timeout="30000"
-    :multi-line="true"
-    @input="closeError"
-    :value="true"
-    >
-{{ error}}
-    <v-btn text dark  @click.native="closeError">Close</v-btn>
-    </v-snackbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    <template v-if="error">
+      <v-snackbar
+        color="dark"
+        :timeout="30000"
+        :multi-line="true"
+        @input="closeError"
+        :value="true"
+      >
+        {{ error}}
+        <v-btn text dark @click.native="closeError">Close</v-btn>
+      </v-snackbar>
     </template>
-    
- 
-
   </v-app>
 </template>
 <script>
-
-import * as fb from 'firebase'
+import * as fb from "firebase";
 
 export default {
-  name: 'App',
+  name: "App",
 
   methods: {
-    closeError(){
-this.$store.dispatch('clearError')
+    closeError() {
+      this.$store.dispatch("clearError");
     },
-     onLogout(){
-this.$store.dispatch('logoutUser')
-.catch((error)=>{
-  console.log(error)
-})
-this.$router.push('/')
-  }
+    onLogout() {
+      this.$store.dispatch("logoutUser").catch(error => {
+        console.log(error);
+      });
+      this.$router.push("/");
+    }
   },
- 
-  computed:{
 
-    categories(){
-     
-return this.$store.getters.getCat
-    
-
-
+  computed: {
+    categories() {
+      return this.$store.getters.getCat;
     },
-    error(){
-return this.$store.getters.error
+    error() {
+      return this.$store.getters.error;
     },
-    isUserLoggedIn(){
-
-
-    return this.$store.getters.isUserLoggedIn
-  }
+    isUserLoggedIn() {
+      return this.$store.getters.isUserLoggedIn;
+    }
   },
 
   data: () => ({
     //
-    idef:'mdi-menu-down',
-    idw:'mdi-menu-down',
-    iup:'mdi-menu-up',
-
-  }),
+    idef: "mdi-menu-down",
+    idw: "mdi-menu-down",
+    iup: "mdi-menu-up"
+  })
 };
 </script>
+
+
 <style  scoped>
-.txt{
+.txt {
   font-size: 50px;
 }
-.c1{
+.c1 {
   height: 550px;
-width: 100%;
-  background-image: url('./img/laptop.jpg');
+  width: 100%;
+  background-image: url("./img/laptop.jpg");
   background-position: center center;
   background-repeat: no-repeat;
   padding: 0;
   background-size: cover;
-
-
 }
-.blackt{
- height: 130px;
- background: rgba(0, 0, 0, 0.75);
- background-size: 100%;
-width: 100%;
-margin-top: 60px;
+.blackt {
+  height: 130px;
+  background: rgba(0, 0, 0, 0.75);
+  background-size: 100%;
+  width: 100%;
+  margin-top: 60px;
 
-
-
-justify-content:center;
+  justify-content: center;
 }
-.comp{
+.comp {
   width: 500px;
   height: 45px;
   background: rgba(128, 128, 128, 0.5);
   font-size: 20px;
-  
 }
-.courses > div{
-  border:1px solid black;
-  height:200px;
-   width:300px;
-   background-size:contain;
-   border-style:none;
-   background-position: center;  
-   
-   background-image: url('https://tadtadya.com/en/wp-content/uploads/sites/2/2017/09/html5-css3.png');
-   background-color:lightgray;
-   
-   
+.courses > div {
+  border: 1px solid black;
+  height: 200px;
+  width: 300px;
+  background-size: contain;
+  border-style: none;
+  background-position: center;
+
+  background-image: url("https://tadtadya.com/en/wp-content/uploads/sites/2/2017/09/html5-css3.png");
+  background-color: lightgray;
 }
-.ft{
-  background:black;
+.ft {
+  background: black;
 }
-.sbox{
-background:   rgba(128, 128, 128, 0.7);
-height:50px;
-width: 500px;
-font-size: 25px;
-padding-left: 30px;
+.sbox {
+  background: rgba(128, 128, 128, 0.7);
+  height: 50px;
+  width: 500px;
+  font-size: 25px;
+  padding-left: 30px;
 }
-.blacktm{
+.blacktm {
   height: 90px;
- background: rgba(0, 0, 0, 0.75);
- background-size: 100%;
- margin-top: 50px;
-width: 100%;
-
+  background: rgba(0, 0, 0, 0.75);
+  background-size: 100%;
+  margin-top: 50px;
+  width: 100%;
 }
-.courses > div>div{
- height: 90px;
- background: rgba(0, 0, 0, 0.75);
- background-size: 100%;
- margin-top: 50px;
-width: 100%;
-text-align: center;
-
+.courses > div > div {
+  height: 90px;
+  background: rgba(0, 0, 0, 0.75);
+  background-size: 100%;
+  margin-top: 50px;
+  width: 100%;
+  text-align: center;
 }
-.logo:hover{
+.logo:hover {
   cursor: pointer;
 }
-.blacktm:hover{
-display: none;
+.blacktm:hover {
+  display: none;
 }
-
-
 </style>
